8a98e12fd0e192f25baa5675567a4f766e0e44da
Marcos P. M. Soares <marcospaulompms@gmail.com>
2025-08-28 00:05:04 -0300

    Corrigido o valor decimal para 2 casas

--- main.dart
+++ main.dart
@@ -2,5 +2,5 @@ void main(){
   double preco = 7.89;
   int quantidadeComprada = 5;
   double total = preco * quantidadeComprada;
-  print("O total a pagar é R\$ $total");
+  print("O total a pagar é R\$ ${total.toStringAsFixed(2)}");
 }
